a9ea8d782b979009a67d4476a7f8304aea14d6f0
rakesh14356~ <rakeshahire430@gmail.com>
2022-10-15 18:56:44 +0530

    done

--- Hello3.java
+++ Hello3.java
@@ -3,7 +3,7 @@ public class Hello3 {
 	public static void main(String[] args) {
 
 		
-System.out.println("my first genkins programm running next what i do please say");
+System.out.println("my first genkins programm running next what i do please say.continuous working cdac");
 	
 		
 	}
